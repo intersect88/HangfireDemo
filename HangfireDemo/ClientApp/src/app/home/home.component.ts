7abe5ed0ae5b2b1af7c2188f951cf872f6bb5c08
intersect88 <intersect@anche.no>
2019-04-02 18:05:34 +0200

    now the app shows only HomePage

--- HangfireDemo/ClientApp/src/app/home/home.component.ts
+++ HangfireDemo/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HeroesService } from './home.service';
+import { Counter } from './counter';
 
 @Component({
-  selector: 'app-home',
+  selector: '',
   templateUrl: './home.component.html',
+  providers: [ HeroesService ]
 })
 export class HomeComponent {
+  public counter = 0;
+  constructor(private heroesService: HeroesService) {
+}
+
+  /** POST: add a new hero to the database */
+  incrementCounter() {
+    const newHero: Counter = { Value: this.counter };
+    this.heroesService.addHero(newHero).subscribe(
+      {
+        complete: () => {
+          this.heroesService.getHeroes().subscribe(
+            c => this.counter = c);
+        }
+      }
+    );
+  }
 }
