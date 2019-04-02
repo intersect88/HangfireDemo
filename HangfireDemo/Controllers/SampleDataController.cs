e18bd30cc13442765960a391b5db1c6a2995f05d
intersect88 <intersect@anche.no>
2019-04-02 14:28:24 +0200

    fix wrong using reference

--- HangfireDemo/Controllers/SampleDataController.cs
+++ HangfireDemo/Controllers/SampleDataController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HangfireDemo.Models;
-using HangfireDemo.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
